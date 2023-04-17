5aa49ce85106c406e91abe2e3dba6a0f12c93217
Ipsita2001 <84337149+Ipsita2001@users.noreply.github.com>
2023-04-17 20:08:26 +0530

    radius of a circle

---
+++ rad of circle.py
@@ -0,0 +1,10 @@
+//rad of circle 1.py
+filename = input("Input the Filename: ")
+f_extns = filename.split(".")
+print ("The extension of the file is : " + repr(f_extns[-1]))
+ 
+//rad of circle 2.py
+from math import pi
+radius = float(input ("Input the radius of the circle : "))
+print ("The area of the circle with radius " + str(radius) + " is: " +
+ str(pi * radius**2))
